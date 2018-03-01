b339e1f4e20b1a8d44e3e490169c7d08ae0c0427
yx <xyang@163.com>
2018-03-01 11:13:32 +0800

    new

---
+++ src/main/java/com/github/jlf1997/api_doc_markup/MarkdownDocs.java
@@ -0,0 +1,78 @@
+package com.github.jlf1997.api_doc_markup;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+
+/**
+ * asciidoctor:process-asciidoc
+ * @author yx
+ *
+ */
+public class MarkdownDocs {
+	
+	
+	private static final String url = "http://localhost:8080/v2/api-docs";
+	 /**
+     * 生成AsciiDocs格式文档
+     * @throws Exception
+     */
+    public static void generateAsciiDocs() throws Exception {
+        //    输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL(url))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("docs/asciidoc/generated/all"));
+    }
+
+    /**
+     * 生成Markdown格式文档
+     * @throws Exception
+     */
+    public static void generateMarkdownDocs() throws Exception {
+        //    输出Markdown格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL(url))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("docs/markdown/generated/all"));
+    }
+    
+    public static void generateConfluenceDocs() throws Exception {
+        //    输出Confluence使用的格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL(url))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("docs/confluence/generated/all"));
+    }
+}
